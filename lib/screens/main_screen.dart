2a40d8f91befaa150ec3550433f3c48d6b5cf9c3
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2024-11-08 01:20:59 +0200

    .

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:taksis/screens/Account/account.dart';
+import 'package:taksis/screens/Orders/orders.dart';
+
+import 'Home Screen/home_page.dart';
+import 'cart/cart.dart';
+import 'wishlist/wish_list.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key, this.index});
@@ -11,27 +17,44 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   late int _selectedIndex;
-  // List of pages for each BottomNavigationBar index
-  // Pass a callback to each page to allow changing the selected index
+
   final List<Widget> _pages = [];
+
+  Future<bool> _handlePop(bool didPop, dynamic result) async {
+    if (didPop) {
+      return false;
+    }
+
+    final shouldExit = await showDialog<bool>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Are you sure?'),
+        content: const Text('Do you want to exit the App'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: const Text('No'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: const Text('Yes'),
+          ),
+        ],
+      ),
+    );
+
+    return shouldExit ?? false;
+  }
 
   @override
   void initState() {
     super.initState();
     _pages.addAll([
-      Scaffold(),
-      Scaffold(),
-      Scaffold(),
-      Scaffold(),
-      Scaffold(),
-      // HomePage(onNavigate: _onItemTapped),
-      // TrendingProducts(onNavigate: _onItemTapped),
-      // // ShopPage(onNavigate: _onItemTapped),
-      // // Profile(onNavigate: _onItemTapped),
-      // ShoppingBag(onNavigate: _onItemTapped),
-      // // Profile(onNavigate: _onItemTapped),
-      // Scaffold(),
-      // Profile(onNavigate: _onItemTapped),
+      HomePage(onNavigate: _onItemTapped),
+      WishList(onNavigate: _onItemTapped),
+      Cart(onNavigate: _onItemTapped),
+      Orders(onNavigate: _onItemTapped),
+      Account(onNavigate: _onItemTapped),
     ]);
     if (widget.index != null) {
       _selectedIndex = widget.index!;
@@ -40,7 +63,6 @@ class _MainScreenState extends State<MainScreen> {
     }
   }
 
-  // Callback function to update selected index
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -49,75 +71,77 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: _pages[_selectedIndex],
-      // Display selected page based on index
-      floatingActionButton: Stack(
-        alignment: Alignment.bottomCenter,
-        children: [
-          Positioned(
-            bottom: 30,
-            child: FloatingActionButton(
-              onPressed: () {
-                _onItemTapped(2); // Set index 2 for the cart page
-              },
-              backgroundColor:
-              _selectedIndex == 2 ? const Color(0xFFD6AA59) : Colors.white,
-              shape: const CircleBorder(),
-              child: Icon(Icons.shopping_cart_outlined,
-                  color: _selectedIndex == 2 ? Colors.white : Color(0xFF45140E)),
+    return WillPopScope(
+      onWillPop: () => _handlePop(false, null),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: _pages[_selectedIndex],
+        floatingActionButton: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Positioned(
+              bottom: 30,
+              child: FloatingActionButton(
+                onPressed: () {
+                  _onItemTapped(2);
+                },
+                backgroundColor: _selectedIndex == 2
+                    ? const Color(0xFFD6AA59)
+                    : Colors.white,
+                shape: const CircleBorder(),
+                child: Icon(Icons.shopping_cart_outlined,
+                    color:
+                        _selectedIndex == 2 ? Colors.white : Color(0xFF45140E)),
+              ),
             ),
-          ),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: Theme(
-        data: Theme.of(context).copyWith(
-          canvasColor: Color(0xFF45140E),
+          ],
         ),
-        child: BottomAppBar(
-          padding: EdgeInsets.zero,
-          notchMargin: 8.0,
-          child: BottomNavigationBar(
-            currentIndex: _selectedIndex,
-            onTap: (index) {
-              if (index != 2) {
-                _onItemTapped(index);
-              }
-            },
-
-            iconSize: 24,
-            selectedItemColor: const Color(0xFFD6AA59),
-            unselectedItemColor: Colors.white,
-            unselectedFontSize: 10,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home_outlined),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.favorite_border_outlined),
-                label: 'Wishlist',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.circle, color: Colors.transparent),
-                label: '', // Placeholder for spacing
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.list_alt),
-                label: 'Orders',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person_pin),
-                label: 'Account',
-              ),
-            ],
-            type: BottomNavigationBarType.fixed,
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(
+            canvasColor: Color(0xFF45140E),
+          ),
+          child: BottomAppBar(
+            padding: EdgeInsets.zero,
+            notchMargin: 8.0,
+            child: BottomNavigationBar(
+              currentIndex: _selectedIndex,
+              onTap: (index) {
+                if (index != 2) {
+                  _onItemTapped(index);
+                }
+              },
+              iconSize: 24,
+              selectedItemColor: const Color(0xFFD6AA59),
+              unselectedItemColor: Colors.white,
+              unselectedFontSize: 10,
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home_outlined),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.favorite_border_outlined),
+                  label: 'Wishlist',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.circle, color: Colors.transparent),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.list_alt),
+                  label: 'Orders',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person_pin),
+                  label: 'Account',
+                ),
+              ],
+              type: BottomNavigationBarType.fixed,
+            ),
           ),
         ),
       ),
     );
   }
 }
-
