8ac84e4613339e9217d464260545f797f2d65d6f
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2024-11-08 01:05:05 +0200

    add some models

---
+++ lib/models/Product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final List<String> images;
+  final String title;
+  final String subtitle;
+  final String details;
+  final double price;
+  final double discount;
+  final int buyers;
+  final double rating;
+
+  Product(this.images, this.title, this.subtitle, this.details, this.price,
+      this.discount, this.buyers, this.rating);
+}
